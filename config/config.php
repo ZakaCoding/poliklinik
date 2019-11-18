e0e26b8eb9c1aacc6136ddb4ced04929a0b150c8
Zaka Noor <zakanoor@outlook.co.id>
2019-11-18 18:37:03 +0700

    Stage 1

---
+++ config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Configuration file for this website
+ * 
+ */
+
+//  disable any error
+// use comment if you want error message turn on
+error_reporting(0);
+
+/**
+ * This is base url
+ * Main page of this page
+ *
+ */
+
+define ('BASE_URL',"http://localhost/poliklinik_um/");
+
+/**
+ * Connection to database
+ * you can costum this setting for connection to database
+ * with your configuration file dbms
+ * this web use MySQL for dbms
+ */
+$mysqli = new mysqli('localhost','root','7cd81f88','poliklinik');
+// if connection has error
+if($mysqli->connect_error)
+{
+    die("Mysql connection error [". $mysqli->connect_errno ."] ".$mysqli->connect_error);
+    // End connection
+    $mysqli->close;
+}
+
---
+++ index.php
@@ -0,0 +1,27 @@
+
+<?php include_once('config/config.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Poliklinik UM</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= BASE_URL . 'vendor/css/bootstrap.min.css'; ?>">
+    <!-- Bootstrap js -->
+    <script src="<?= BASE_URL . 'vendor/js/bootstrap.min.js' ?>"></script>
+</head>
+<body>
+        
+
+
+
+    <!-- Javascript HERE -->
+    <!-- Jquery -->
+    <script src="<?= BASE_URL . 'vendor/js/jquery-3.3.1.slim.min.js' ?>"></script>
+    <!-- Popper JS -->
+    <script src="<?= BASE_URL . 'vendor/js/popper.min.js' ?>"></script>
+</body>
+</html>
