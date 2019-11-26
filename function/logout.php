9c7930e724bb6a1543a1581066c26b2d3d69c2b1
Zaka Noor <zakanoor@outlook.co.id>
2019-11-26 21:08:33 +0700

    add new feature on logout

--- function/logout.php
+++ function/logout.php
@@ -1,6 +1,17 @@
 <?php
     session_start();
-    session_unset();
-    session_destroy();
-    // Redirect to landing page
-    header("location: http://localhost/poliklinik_um");
+    include_once '../config/config.php';
+
+    // clearly token when user logout
+    $mysqli->query("UPDATE users SET remember_token = '' WHERE email = '". $_SESSION['user']['email'] ."'");
+    if($mysqli->affected_rows)
+    {
+        session_unset();
+        session_destroy();
+        // Redirect to landing page
+        header('location: '.BASE_URL);
+    }
+    else
+    {
+        // No else i mean :)
+    }
