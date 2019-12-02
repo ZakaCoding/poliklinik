9be2adfb36bfd60a74e7dc96ecbf64684081e25c
Zaka Noor <zakanoor@outlook.co.id>
2019-12-02 21:09:43 +0700

    Add new table for database poliklinik

--- config/database/reservasi.sql
+++ config/database/reservasi.sql
@@ -1,12 +1,13 @@
 -- Create a new table called 'users'
 -- Drop the table if it already exists
 CREATE TABLE `tbl_reservasi` (
-  `id` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `poli_category` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reservation_id` varchar(13) NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `poli_category` ENUM('Poli Umum','Poli Gigi', 'Poli Ibu dan Anak') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` INT(3) COLLATE utf8mb4_unicode_ci NOT NULL,
   `reservased_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_reservased_unique` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+  PRIMARY KEY `reservation_id` (`reservation_id`),
+  UNIQUE KEY `id_reservased_unique` (`reservation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
